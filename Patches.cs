44d8c0d3ac44113bf5fbcfd0ffa952a1d807b835
Brad <bradleyglaurie@gmail.com>
2023-04-18 09:17:33 +0100

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CheaperWorkPhone
+{
+	internal class Patches
+	{
+		private static readonly int OriginalSupplyVan = -Economy.orderSupplyVan;
+		private static readonly int OriginalPrison = -Economy.orderPrisonVan;
+
+		[HarmonyPatch(typeof(PostHouse), "OnUpgraded")]
+		[HarmonyPostfix]
+		private static void PostHouseUpgraded()
+		{
+			CheaperWorkPhone.Log.LogInfo("PostHouse upgraded");
+			var postHouse = GameObject.FindObjectOfType<PostHouse>();
+			var currentLevel = postHouse.GetLevel();
+
+			int newSupplyCost = OriginalSupplyVan - ((currentLevel - 1) * 50);
+			int newPrisonCost = OriginalPrison - ((currentLevel - 1) * 125);
+
+			Economy.orderSupplyVan = -newSupplyCost;
+			Economy.orderPrisonVan = -newPrisonCost;
+			CheaperWorkPhone.Log.LogInfo($"Supply van now costs ${newSupplyCost}, prison van now costs ${newPrisonCost}");
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace CheaperWorkPhone
+{
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInProcess("ContrabandPolice.exe")]
+	public class CheaperWorkPhone : BaseUnityPlugin
+	{
+		internal static ManualLogSource Log;
+
+		private void Awake()
+		{
+			Log = Logger;
+			Harmony.CreateAndPatchAll(typeof(Patches));
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+		}
+	}
+}
